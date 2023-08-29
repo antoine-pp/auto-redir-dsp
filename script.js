116bd94fb62bab1d03ae524aa7882c4e8c6f020c
Antoine Masson <a.masson91@gmail.com>
2023-08-29 17:57:56 +0200

    1.3

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// https://antoine-pp.github.io/auto-redir-dsp/
+
+// Add this function to set a cookie
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Add this function to get a cookie
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i=0;i < ca.length;i++) {
+        let c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function logDebug(message, cookieAction = false, cookieContent = null) {
+    const debugLog = document.getElementById('debugLog');
+    const timestamp = new Date().toLocaleTimeString();
+    
+    if (cookieAction) {
+      debugLog.innerHTML += `<h2>${cookieAction}</h2>`;
+      debugLog.innerHTML += `<p>Time: ${timestamp}</p>`;
+      debugLog.innerHTML += `<p>Cookie Content: ${cookieContent}</p>`;
+    } else {
+      debugLog.innerHTML += `<p>${timestamp} - ${message}</p>`;
+    }
+  }
+
 document.addEventListener('DOMContentLoaded', function() {
     const userAgent = navigator.userAgent.toLowerCase();
     const isIOS = /iphone|ipad|ipod/.test(userAgent);
@@ -28,33 +66,37 @@ document.addEventListener('DOMContentLoaded', function() {
   
     function openOrTryNextApp() {
         let startTime = Date.now();
-        let timeout = 1000; // increased from 25 to 1000
+        let timeout = 1000;
         const app = apps[currentIndex];
-        
         const deepLink = isIOS ? app.iosDeepLink : isAndroid ? app.androidDeepLink : null;
-
+    
         if (deepLink) {
-            logDebug(`Trying to open: ${deepLink}`);
-            window.location = deepLink;
-
-            setTimeout(function() {
+          logDebug(`Trying to open: ${deepLink}`);
+          window.location = deepLink;
+    
+          setTimeout(function() {
             if (Date.now() - startTime < 100 + timeout) {
-                if (currentIndex < apps.length - 1) {
+              if (currentIndex < apps.length - 1) {
                 currentIndex++;
+                logDebug(`Could not open: ${deepLink}`);
                 openOrTryNextApp();
-                }
+              }
+            } else {
+              setCookie('lastWorkingDeepLink', deepLink, 30); // Save the deep link in a cookie for 30 days
+              logDebug(null, "Cookie has been created", deepLink);
             }
-            }, timeout);
+          }, timeout);
         } else {
-            logDebug('Device not supported.');
+          logDebug('Device not supported.');
         }
     }
-
-    function logDebug(message) {
-        const debugLog = document.getElementById('debugLog');
-        debugLog.innerHTML += `<p>${message}</p>`;
+    
+    const lastWorkingDeepLink = getCookie('lastWorkingDeepLink');
+    if (lastWorkingDeepLink) {
+        window.location = lastWorkingDeepLink; // Open the last working deep link if available
+        logDebug(null, "Cookie has been read", lastWorkingDeepLink);
+    } else {
+        openOrTryNextApp();
     }
-  
-    openOrTryNextApp();
-  });
+});
   
